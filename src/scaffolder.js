655b568c8ecd8764ed349243d5adf18ee7f50dbf
Matt Travi <programmer@travi.org>
2019-03-30 22:04:22 -0500

    feat(packages): defined deps, devDeps, and scripts

--- src/scaffolder.js
+++ src/scaffolder.js
@@ -1,3 +1,24 @@
 export function scaffold() {
-  return {dependencies: ['spectacle']};
+  return {
+    dependencies: ['spectacle', 'react', 'react-dom', 'prop-types', 'normalize.css'],
+    devDependencies: [
+      'webpack',
+      'webpack-cli',
+      'webpack-config-utils',
+      'webpack-dev-server',
+      'react-hot-loader',
+      'redbox-react',
+      'html-webpack-plugin',
+      'babel-loader',
+      'mustache-loader',
+      'raw-loader',
+      'style-loader'
+    ],
+    scripts: {
+      build: 'webpack --env production',
+      'build:dev': 'webpack --env development',
+      start: 'webpack-dev-server'
+    },
+    vcsIgnore: {files: [], directories: []}
+  };
 }
--- test/unit/scaffolder-test.js
+++ test/unit/scaffolder-test.js
@@ -3,6 +3,30 @@ import {scaffold} from '../../src/scaffolder';
 
 suite('scaffolder', () => {
   test('that spectacle dependencies are defined', async () => {
-    assert.deepEqual(await scaffold().dependencies, ['spectacle']);
+    assert.deepEqual(
+      await scaffold(),
+      {
+        dependencies: ['spectacle', 'react', 'react-dom', 'prop-types', 'normalize.css'],
+        devDependencies: [
+          'webpack',
+          'webpack-cli',
+          'webpack-config-utils',
+          'webpack-dev-server',
+          'react-hot-loader',
+          'redbox-react',
+          'html-webpack-plugin',
+          'babel-loader',
+          'mustache-loader',
+          'raw-loader',
+          'style-loader'
+        ],
+        scripts: {
+          build: 'webpack --env production',
+          'build:dev': 'webpack --env development',
+          start: 'webpack-dev-server'
+        },
+        vcsIgnore: {files: [], directories: []}
+      }
+    );
   });
 });
